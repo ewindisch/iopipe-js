2e5ffc60abc22abcc837788a96e9d4d47d1ca21e
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2019-02-27 23:08:10 -0500

    Cleanup localtest.sh
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- .circleci/localtest.sh
+++ .circleci/localtest.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-__exedir="$(readlink -f -- "$(dirname -- "$0")")"
+__gitdir=$(dirname -- $(dirname -- "$0"))
 __measuretmp=$(mktemp -d) || exit 1
 MEASURE_REPO=${MEASURE_REPO:-git@github.com:iopipe/measure.git}
-git clone $MEASURE_REPO "$__measuretmp"
+git clone "$MEASURE_REPO" "$__measuretmp"
 
 __runtmp=$(mktemp -d) || exit 1
-cp -R * $__runtmp/  # copy .circleci dir to $__runtmp
-cp $__measuretmp/measure/data/sls-nodejs8.10.yml serverless.yml
+cp -R $__gitdir/.circleci/* $__runtmp/  # copy .circleci dir to $__runtmp
 cd $__runtmp
-npm install "$__exedir/.."" # or whereever the iopipe-js dir is
+npm install "$__exedir/.." # or whereever the iopipe-js dir is
 # We want to call the runmeasure script but from inside the sls project for the baseline handler!
+cp $__measuretmp/measure/data/sls-nodejs8.10.yml serverless.yml
 $__measuretmp/runmeasure.sh -l nodejs8.10 -h baseline.handler
 
 rm -rf "$__measuretmp"
